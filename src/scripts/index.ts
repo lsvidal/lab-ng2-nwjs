4ae157fac96ad0c4638108804a0df537da2efcc1
Leonardo Vidal <leonardo73vidal@gmail.com>
2015-06-15 15:45:33 -0300

    Example expanded to use a script.

---
+++ src/scripts/index.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+window.onload = function () {
+	var process = require('process');
+	var doc = document.getElementById('info');
+	var platform = process.platform;
+	var arch = process.arch;
+	var msg = 'unknown';
+	if (arch === 'x64') {
+		if (platform === 'win32') {
+			msg = 'Windows 64 bits';
+		} else {
+			msg = 'Linux 32 bits';
+		}
+	} else {
+		if (platform === 'win32') {
+			msg = 'Windows 32 bits';
+		} else {
+			msg = 'Linux 32 bits';
+		}
+	}
+	doc.innerHTML = 'Current arch is ' + msg;
+};
